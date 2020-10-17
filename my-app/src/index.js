a49bd58c07a4d6e522768609a960398bc2c8cf30
Lyra <lad279@cornell.edu>
2020-10-16 18:23:37 -0700

    Functionality for taking turns.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -19,13 +16,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -38,7 +39,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' +
+      (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
