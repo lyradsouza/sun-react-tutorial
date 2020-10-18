76fbe3c70968edac4e3383df79cc0f65bdfb1f70
Lyra <lad279@cornell.edu>
2020-10-17 21:34:16 -0700

    Alternate turns and check for winnner.

--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -13,16 +13,30 @@ function Square({ value, onClick }) {
 
 function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const winner = calculateWinner(squares);
 
   function renderSquare(i) {
     return <Square
       value={squares[i]}
       onClick={() => {
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
+        nextSquares[i] = isXNext ? "X" : "O";
         setSquares(nextSquares);
+
+        setIsXNext(!isXNext); // toggle turns
       }}
     />;
+  }
+
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + (isXNext ? "X" : "O");
+    }
   }
 
   return (
@@ -45,9 +59,41 @@ function Game() {
             {renderSquare(8)}
           </div>
         </div>
+        <div className="game-info">{getStatus()}</div>
       </div>
     </div>
   );
 }
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  // go over all possibly winning lines and check if they consist of only X's/only O's
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
