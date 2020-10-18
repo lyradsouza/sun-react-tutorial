ffe08927676c23b6182ece8962381e20a0641d69
Lyra <lad279@cornell.edu>
2020-10-17 21:17:49 -0700

    Created game board.

--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -11,4 +11,31 @@ function Square({ value, onClick }) {
   );
 }
 
-ReactDOM.render(<Square />, document.getElementById("root"));
+function Game() {
+
+  return (
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            <Square value={0} onClick={null} />
+            <Square value={1} onClick={null} />
+            <Square value={2} onClick={null} />
+          </div>
+          <div className="board-row">
+            <Square value={3} onClick={null} />
+            <Square value={4} onClick={null} />
+            <Square value={5} onClick={null} />
+          </div>
+          <div className="board-row">
+            <Square value={6} onClick={null} />
+            <Square value={7} onClick={null} />
+            <Square value={8} onClick={null} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
